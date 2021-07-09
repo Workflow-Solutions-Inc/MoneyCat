7e5447c7e3a90c2dd3dc63a3f8faa3d4800f3e49
jonaldarjoncruyz <82009738+jonaldarjoncruyz@users.noreply.github.com>
2021-07-09 11:19:36 +0800

    change in key and secret

--- process/controllers/config/xeroconfig.php
+++ process/controllers/config/xeroconfig.php
@@ -1,8 +1,8 @@
 <?php
 
 	$callback = "https://test-moneycat-uploader.sypro-it.com/process/controllers/callback.php";
-	$clientid = "8FB8007E4B4F4EF4ADE4FF6918D591F5";
-	$clientsecret = "iXq3dwaGi8cNmTKupfdGrdl9qwMbQI4D7fmx4BsysZZrUDMx";
+	$clientid = "0C9451D86618432994D37A8DA4F80FFF";
+	$clientsecret = "nr3JlfYQHR3SZpOzB6jC5c72qPiL3pwh8MuOIhjUsdHLquk1";
 	
 	
 	//$base_path = "";
