a02072d7970d5eb7c0c8410b422cd631d399d143
jonaldarjoncruyz <82009738+jonaldarjoncruyz@users.noreply.github.com>
2021-07-09 11:14:21 +0800

    change callback

--- process/controllers/config/xeroconfig.php
+++ process/controllers/config/xeroconfig.php
@@ -1,6 +1,6 @@
 <?php
 
-	$callback = "http://localhost/moneycat/process/controllers/callback.php";
+	$callback = "https://test-moneycat-uploader.sypro-it.com/process/controllers/callback.php";
 	$clientid = "8FB8007E4B4F4EF4ADE4FF6918D591F5";
 	$clientsecret = "iXq3dwaGi8cNmTKupfdGrdl9qwMbQI4D7fmx4BsysZZrUDMx";
 	
