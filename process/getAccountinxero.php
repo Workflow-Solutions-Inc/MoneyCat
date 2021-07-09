c707d69265524a9a8704d08726c95a0a70267b8c
jonaldarjoncruyz <82009738+jonaldarjoncruyz@users.noreply.github.com>
2021-07-09 12:55:01 +0800

    Update getAccountinxero.php

--- process/getAccountinxero.php
+++ process/getAccountinxero.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 'On');
   //require __DIR__ . '/vendor/autoload.php';
   //require_once('xeroconfig.php');
   require_once('controllers/config/xeroconfig.php');
-  require_once('C:\Users\SysDev - PC3\vendor\autoload.php');
+require_once('vendor/autoload.php');
   require_once('controllers/storage.php');
   include_once('controllers/customer.php');
 
