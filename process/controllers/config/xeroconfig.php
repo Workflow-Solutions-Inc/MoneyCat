f4433d134a4d59f0afc01ebb07cfc1b27223c1aa
jonaldarjoncruyz <82009738+jonaldarjoncruyz@users.noreply.github.com>
2021-07-10 13:18:56 +0800

    Update xeroconfig.php

--- process/controllers/config/xeroconfig.php
+++ process/controllers/config/xeroconfig.php
@@ -2,7 +2,7 @@
 
 	$callback = "https://test-moneycat-uploader.sypro-it.com/process/controllers/callback.php";
 	$clientid = "0C9451D86618432994D37A8DA4F80FFF";
-	$clientsecret = "nr3JlfYQHR3SZpOzB6jC5c72qPiL3pwh8MuOIhjUsdHLquk1";
+	$clientsecret = "t4ImIo7EwEb3tsnMFYsR-LITaO19BypY4XESqj6kvdMg7m26";
 	
 	
 	//$base_path = "";
