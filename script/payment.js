54e78299af000c424942a8ea61d5ed554007720a
jonaldarjoncruyz <82009738+jonaldarjoncruyz@users.noreply.github.com>
2021-07-14 17:13:07 +0800

    Update payment.js

--- script/payment.js
+++ script/payment.js
@@ -26,10 +26,41 @@ fileToRead.addEventListener("change", function(event) {
 
 }, false);
 
+$(function () {
+    var pleaseWait = $('#pleaseWaitDialog'); 
+    
+    showPleaseWait = function() {
+        $('#pleaseWaitDialog').modal({backdrop: 'static', keyboard: false}) 
+        pleaseWait.modal('show');
 
+    };
+        
+    hidePleaseWait = function () {
+        pleaseWait.modal('hide');
+    };
+    
+    //showPleaseWait();
+});
+
+$(function () {
+    var pleaseWait = $('#pleasereconnectmodal'); 
+    
+    showPleaseWait2 = function() {
+        $('#pleasereconnectmodal').modal({backdrop: 'static', keyboard: false}) 
+        pleaseWait.modal('show');
+
+    };
+        
+    hidePleaseWait2 = function () {
+        pleaseWait.modal('hide');
+    };
+    
+    //showPleaseWait();
+});
 
 function splitJson(jsonParams)
 {
+    showPleaseWait();
     var json = $.parseJSON(jsonParams);
     var currentcount = 1;
     for (var i=0;i< json.length;++i)
@@ -46,41 +77,8 @@ function splitJson(jsonParams)
             amount_type = json[i].amount_type;
 
             if(category == 2){
-                var action = "postdata";
-            $.ajax({
-                        type: 'POST',
-                        url: 'process/paymentprocess2.php',
-                        data:{action:action,
-                          contact_Id:contact_Id,
-                          agreement_number:agreement_number,
-                          loan_description:loan_description,
-                          loan_amount:loan_amount,
-                          account:account,
-                          category:category,
-                          date_of_payment:date_of_payment,
-                          amount_type:amount_type
-                        },
-
-                         beforeSend:function(){
-
-                            document.getElementById("btnupload").disabled = true;
-                            document.getElementById("btnupload").innerHTML = '<i class="fa fa-spinner fa-spin" aria-hidden="true"></i> Loading';
-                        },
-                        success: function(data){
-                            
-                            //alert("done");
-                            document.getElementById("btnupload").disabled = true;
-                            //document.getElementById("btnupload").innerHTML = "Upload";
-                            document.getElementById("uploadresult").innerHTML +="<div style='margin-left:20px;color:grey;'>"+data+"</div><hr>";
-                            currentcountjson(currentcount);
-                            currentcount+=1;
-                            
-
-
-                          
-                         }
-                        
-                });
+                uploadpayments(contact_Id,agreement_number,loan_description,loan_amount,account,category,date_of_payment,amount_type);
+                getLines();
             }
      
         }
@@ -99,47 +97,37 @@ function splitJson(jsonParams)
             amount_type = json[i].amount_type;
 
             if(category != 2){
-                var action = "postdata";
-            $.ajax({
-                        type: 'POST',
-                        url: 'process/paymentprocess2.php',
-                        data:{action:action,
-                          contact_Id:contact_Id,
-                          agreement_number:agreement_number,
-                          loan_description:loan_description,
-                          loan_amount:loan_amount,
-                          account:account,
-                          category:category,
-                          date_of_payment:date_of_payment,
-                          amount_type:amount_type
-                        },
-                        beforeSend:function(){
-
-                            document.getElementById("btnupload").innerHTML = "Loading..";
-                        },
-
-                         beforeSend:function(){
-
-                            document.getElementById("btnupload").disabled = true;
-                            document.getElementById("btnupload").innerHTML = '<i class="fa fa-spinner fa-spin" aria-hidden="true"></i> Loading';
-                        },
-                        success: function(data){
-                            
-                            //alert("done");
-                            document.getElementById("btnupload").disabled = true;
-                            //document.getElementById("btnupload").innerHTML = "Loading";
-                            document.getElementById("uploadresult").innerHTML +="<div style='margin-left:20px;color:grey;'>"+data+"</div><hr>";
-                            currentcountjson(currentcount);
-                            currentcount+=1;
-                            
-
-                          
-                         }
-                        
-                });
+                uploadpayments(contact_Id,agreement_number,loan_description,loan_amount,account,category,date_of_payment,amount_type);  
+                getLines();          
             }
      
         }
+    
+}
+
+function uploadpayments(contact_Id,agreement_number,loan_description,loan_amount,account,category,date_of_payment,amount_type){
+    var action = "postdata";
+    $.ajax({
+                type: 'POST',
+                url: 'process/paymentprocess2.php',
+                data:{action:action,
+                  contact_Id:contact_Id,
+                  agreement_number:agreement_number,
+                  loan_description:loan_description,
+                  loan_amount:loan_amount,
+                  account:account,
+                  category:category,
+                  date_of_payment:date_of_payment,
+                  amount_type:amount_type
+                },
+
+                 beforeSend:function(){
+                },
+                success: function(data){
+                    // document.getElementById("uploadresult").innerHTML +="<div style='margin-left:20px;color:grey;'>"+data+"</div><hr>";
+                 }
+                
+        });
 }
 
 function currentcountjson(count){
@@ -152,17 +140,7 @@ function upload(){
 	}else{
         clearjson()
         document.getElementById("uploadresult").innerHTML = "";
-		splitJson(formatted);
-
-        //alert("Done");
-        setTimeout(function(){
-        getLines();
-        document.getElementById("btnupload").disabled = false;
-        //alert("Done");
-        },3000);
-        
-        
-
+		validateconnectiontoapi(formatted);
 	}
 	
 }
@@ -192,9 +170,29 @@ function getLines(){
             },
             success: function(data){
                 console.log(data);
+                hidePleaseWait();
                 //document.getElementById("uploadresult").innerHTML += data;
              }
             
+    });
+}
+
+function validateconnectiontoapi(formatted){
+    $.ajax({
+        type: 'GET',
+        url: 'process/checkconnection.php',
+        data:{},
+        beforeSend:function(){        
+
+        },
+        success: function(data){
+            if(data==1){
+                splitJson(formatted);
+            }else{
+                showPleaseWait2();
+            }   
+        }
+        
     });
 
 }
