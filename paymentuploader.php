f33eea38aee7351194f86855c2fdd08243bfe7ca
jonaldarjoncruyz <82009738+jonaldarjoncruyz@users.noreply.github.com>
2021-07-15 14:51:52 +0800

    Update paymentuploader.php

--- paymentuploader.php
+++ paymentuploader.php
@@ -57,7 +57,9 @@ include("process/controllers/config/dbconn.php");
                                                     <i class="pe-7s-date"></i>   <label>Choose File</label>
                                                     <input type="File" name="myjson" id="myjson" class="form-control" required>
                                                     <br>
-                                                    <button onclick="upload()" class="btn  btn-fill pull-right" id="btnupload">Upload</button>
+                                                    <button onclick="upload()" class="btn  btn-fill pull-right" style="margin-left: 20px;" id="btnupload">Upload</button>
+
+                                                    <button onclick="validate()" class="btn  btn-fill pull-right" id="btnupload">Validate</button>
                                                 </div>
                                             </div>
 
@@ -80,7 +82,7 @@ include("process/controllers/config/dbconn.php");
                             </pre>
                         </div>
                     </div>
-                    <div class="col-md-12" style="display:none;">
+                    <div class="col-md-12" style="display: none;">
                         <div class="card">
                             <div class="header">
                                 <h4 class="title">Upload Logs</h4>
@@ -90,6 +92,11 @@ include("process/controllers/config/dbconn.php");
                             <br>
                             <pre style="overflow-y: scroll; height: 400px;" id="uploadresult">
                             </pre>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div>
+                            <button onclick="showPleaseWait3();" class="btn  btn-fill pull-right">Upload Logs</button>
                         </div>
                     </div>
                 </div>
@@ -127,6 +134,25 @@ include("process/controllers/config/dbconn.php");
   </div>
 </div>
 
+<div class="modal fade" id="logsmodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
+  <div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h3>Upload Logs</h3>
+        </div>
+      <div class="modal-body">
+        <div id="testresult" style="overflow-y: scroll; height: 600px;margin : 0;">
+            <dl id="testresult">
+            </dl>
+        </div>
+      </div>
+      <div class="modal-footer">
+          <button onclick="hidePleaseWait3()" class="btn  btn-danger pull-right">close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
     <!--   Core JS Files   -->
     <script src="assets/js/jquery.3.2.1.min.js" type="text/javascript"></script>
     <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
@@ -147,7 +173,10 @@ include("process/controllers/config/dbconn.php");
 
 
     <script type="text/javascript">
-
+        window.onload = function(value){
+        //synccustomer();
+        document.getElementById("btnupload").disabled = true;
+        }
     </script>
 
     </html>
