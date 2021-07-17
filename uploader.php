0e7d3118484a88a6f82ba2c2ba92d56bbff050a6
jonaldarjoncruyz <82009738+jonaldarjoncruyz@users.noreply.github.com>
2021-07-17 09:40:13 +0800

    Update uploader.php

--- uploader.php
+++ uploader.php
@@ -1,188 +1,129 @@
-<?php 
+<?php
 require_once('inc/sidebarexcelreports.php');
 require_once('inc/header.php');
 include("process/controllers/config/dbconn.php");
 ?>
-
 <!doctype html>
-<html lang="en">
-<head>
+    <html lang="en">
+    <style>
+    #myProgress {
+      width: 100%;
+      background-color: #ddd;
+    }
 
-    <meta charset="utf-8" />
-    <link rel="icon" type="image/png" href="assets/img/favicon.png">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    #myBar {
+      width: 10%;
+      height: 30px;
+      background-color: #04AA6D;
+      text-align: center;
+      line-height: 30px;
+      color: white;
+    }
+    </style>
+    <head>
 
-    <title>Setup</title>
+        <meta charset="utf-8" />
+        <link rel="icon" type="image/png" href="assets/img/favicon.png">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-    <meta name="viewport" content="width=device-width" />
+        <title>Contacts - Upload your contact to xero</title>
+
+        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+        <meta name="viewport" content="width=device-width" />
 
 
-    <!-- Bootstrap core CSS     -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+        <!-- Bootstrap core CSS     -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
 
-    <!-- Animation library for notifications   -->
-    <link href="assets/css/animate.min.css" rel="stylesheet"/>
+        <!-- Animation library for notifications   -->
+        <link href="assets/css/animate.min.css" rel="stylesheet"/>
 
-    <!--  Light Bootstrap Table core CSS    -->
-    <link href="assets/css/light-bootstrap-dashboard.css?v=1.4.0" rel="stylesheet"/>
-  <link href="assets/css/demo.css" rel="stylesheet" />
-    <!--     Fonts and icons     -->
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
-    <link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
+        <!--  Light Bootstrap Table core CSS    -->
+        <link href="assets/css/light-bootstrap-dashboard.css?v=1.4.0" rel="stylesheet"/>
+        <link href="assets/css/demo.css" rel="stylesheet" />
+        <!--     Fonts and icons     -->
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+        <link href='https://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+        <link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
 
-    <!-- js  -->
-    <style type="text/css">
-    
-</style>
-
-</head>
-<body>
-
-<div class="wrapper">
-    <div class="main-panel">
-        <div class="content">
-            <div class="container-fluid">
-                <div class="row">
-                      <div class="col-md-12">
-                            <div class="card">
-                                <div class="header">
-                                    <h4 class="title">Loan Disbursement Setup</h4>
-                                    <p class="category">Note: Setup all the necessary data before uploading a json file.</p>
-                                </div>
-                                <div class="content">
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <div class="form-group">
-                                              <i class="pe-7s-date"></i>   <label>Tax Rates</label>
-                                                <select class="form-control" onclick="validateconnectiontoapi();" id="taxrates" onchange="updateTaxrate()">
-                                                    
-                                                </select>
-                                            </div>
-                                            <br>
-                                            <div class="form-group">
-                                                <i class="pe-7s-date"></i> <label>Add Disbursement Channel</label>
-                                                <br>
-                                                <div class="col-md-12">
-                                                    <div class="col-md-4"><input class="form-control" type="text" placeholder="Account code" id="loanaccountcode"></div>
-                                                    <div class="col-md-4">
-                                                        <select class="form-control" id="xerochannel">
-                                                        </select>
-                                                    </div>
-                                                    <div class="col-md-2"><input class="btn  btn-fill pull-left" type="button" value="Save Channel" onclick="saveLoanChannel()"></div>      
-                                                </div>
-                                            </div>
-                                            <br>
-                                            <br>
-                                            <div class="form-group">
-                                                <i class="pe-7s-date"></i>   <label>List of Disbursement Channel</label>
-                                                <br>
-                                                <div style="height:400px; overflow-y: scroll;" id="listofloanchannel">
-                                              
-                                            </div>
-                                            </div>
-
-                                        </div>
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="main-panel">
+                <div class="content">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="card">
+                                    <div class="header">
+                                        <h4 class="title">Contacts</h4>
+                                        <p class="category">Note: Please select a json file extension</p>
                                     </div>
-                                </div>     
-                            </div>
-                        </div>
-                        <div class="col-md-12">
-                            <div class="card">
-                                <div class="header">
-                                    <h4 class="title">Payment Setup</h4>
-                                    <p class="category">Note: Setup all the necessary data before uploading a json file.</p>
-                                </div>
-                                <div class="content">
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <div class="form-group" style="display: none;">
-                                              <i class="pe-7s-date"></i>   <label>Tax Rates</label>
-                                                <select class="form-control"  onclick="validateconnectiontoapi();" id="taxrates2" onchange="updateTaxrate2()">
-                                                    
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-12">
-                                            <div class="form-group">
-                                                <i class="pe-7s-date"></i> <label>Select Bank Account</label>
-                                                <br>
-                                                <div class="col-md-12">
-                                                    <div class="col-md-12">
-                                                        <select class="form-control"  onclick="validateconnectiontoapi();" id="paymentchannel" onchange="updatePaymentChannel()">
-                                                        </select>
-                                                    </div>     
-                                                </div>
-                                            </div>                                       
-                                            <div class="form-group" style="display: none;">
-                                                <i class="pe-7s-date"></i><label>List of Disbursement Channel</label>
-                                                <br>
-                                                <div style="height:400px; overflow-y: scroll;" id="listofpaymentchannel">
-                                              
-                                            </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-12">
-                                            <div class="form-group">
-                                              <i class="pe-7s-date"></i>   <label>Add Accounts</label>
-                                              <br>
-                                                <div class="col-md-12">
-                                                    <div class="col-md-3">
-                                                        <select class="form-control" id="accountcode">
-                                                    
-                                                        </select>
-                                                    </div>
-                                                    <div class="col-md-3"><input class="form-control" type="text" id ="accountname" placeholder="Account name" ></div>
-                                                    <div class="col-md-3" style="display:none;"><input class="form-control" type="text" id ="accountdesc" placeholder="Description" ></div>
-                                                    <div class="col-md-3">
-                                                        <select class="form-control" id="accounttype">
-                                                            <option value = "2">Receive Payment</option>
-                                                            <option value = "1">Invoice Payment</option>
-                                                        </select>
-                                                    </div>
-                                                    <div class="col-md-3"><input class="btn  btn-fill pull-left" type="button" value="Save Account" onclick="addupdate()"></div>      
-                                                </div>
-                                            </div>
-                                            
-                                            <div class="form-group">
-                                                <i class="pe-7s-date"></i><label>List of Accounts</label>
-                                                <br>
-                                                <div style="height:480px; overflow-y: scroll;" id="listofaccounts" >
-                                              
-                                                </div>
-                                            </div>
+                                    <div class="content">
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="form-group">
+                                                  <i class="pe-7s-date"></i>   <label>Choose File</label>
+                                                  <input type="File" name="myjson" id="myjson" class="form-control" required>
+                                                  <br>
+                                                  <button onclick="upload()" class="btn  btn-fill pull-right" style="margin-left: 20px;" id="btnupload">Upload</button>
 
-                                            <div class="form-group">
-                                                <i class="pe-7s-date"></i><label>Overpayment Setup</label>
-                                                <br>
-                                                <div class="col-md-12">
-                                                        <select class="form-control" onclick="validateconnectiontoapi();" id="accountcodeoverpayment" onchange="updateoverpaymentchannel()">
-                                                    
-                                                        </select>
-                                                    </div>
-                                              
-                                                    
-                                            </div>
-                                            </div>
-                                            
-                                        </div>
-                                    </div>
-                                </div>     
-                            </div>
+                                                    <button onclick="validate()" class="btn  btn-fill pull-right" id="btnupload">Validate</button>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>     
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-md-12" style="display: none;">
+                    <div class="card">
+                        <div class="header">
+                            <h4 class="title">Contact Json</h4>
                         </div>
+                        <pre style="overflow-y: scroll; height: 400px;" id="result">
+                        </pre>
+                    </div>
+                </div>
+                <div class="col-md-12" style="display:none;">
+                    <div class="card">
+                        <div class="header">
+                            <h4 class="title">Upload Logs</h4>
+                            <hr>
+                            <h5 id="progressbarid" class="title">Progress: <b id="currentjsonupload">1</b> out of <b id="totaljsondata">1</b></h5>
+                        </div>
+                        <br>
+                        <pre style="overflow-y: scroll; height: 400px;" id="uploadresult">
+                        </pre>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div>
+                        <button onclick="showPleaseWait3();" class="btn  btn-fill pull-right">Upload Logs</button>
+                    </div>
                 </div>
             </div>
         </div>
-        <?php include("inc/footer.php"); ?>
     </div>
-</div>
-
-
-
 
 </body>
 
+<!-- Modal -->
+<div class="modal fade" id="pleaseWaitDialog" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-body">
+        <!-- <h3 style="color:seagreen;"><i id="progresslabel">Processing..</i> <i class="fa fa-spinner fa-spin fa-1x fa-fw"></i>
+        <span class="sr-only">Loading...</span></h3> -->
+        <h3 style="color:seagreen;" id = "overalllabel"><i id="progresslabel">Processing..</i> <iframe src="https://giphy.com/embed/sSgvbe1m3n93G" width="100%" height="50%" frameBorder="0" class="giphy-embed"></iframe></h3>
+        <div>
+            
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 <div class="modal fade" id="pleasereconnectmodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
   <div class="modal-dialog">
@@ -202,11 +143,28 @@ include("process/controllers/config/dbconn.php");
   </div>
 </div>
 
+<div class="modal fade" id="logsmodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
+  <div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h3>Upload Logs</h3>
+        </div>
+      <div class="modal-body">
+        <div id ="resultlabel"></div>
+        <hr>
+        <div id="testresult" style="overflow-y: scroll; height: 600px;margin : 0;">
+            <dl id="testresult">
+            </dl>
+        </div>
+      </div>
+      <div class="modal-footer">
+          <button onclick="hidePleaseWait3()" class="btn  btn-danger pull-right">close</button>
+      </div>
+    </div>
+  </div>
+</div>
 
-
-
-
-</html>
+<!--   Core JS Files   -->
 <script src="assets/js/jquery.3.2.1.min.js" type="text/javascript"></script>
 <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
 
@@ -221,16 +179,18 @@ include("process/controllers/config/dbconn.php");
 
 <!-- Light Bootstrap Table DEMO methods, don't include it in your project! -->
 <script src="assets/js/demo.js"></script>
-<script type="text/javascript" src="script/setup.js" ></script>
+<script type="text/javascript" src="script/customer.js" ></script>
+
+
+
 <script type="text/javascript">
     window.onload = function(value){
-        getTaxrates();
-        getListofAccounts();
-        getListofLoanChannel();
-        getListofPaymentChannel();
-        getListofLoanXeroChannel();
-        getPaymentChannelList();
-        getxeroaccounts();
-        getselectedopchannel();
+        //synccustomer();
+        document.getElementById("btnupload").disabled = true;
+        validateconnectiontoapi2();
     }
+
+    
 </script>
+
+</html>
